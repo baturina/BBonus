de713ec9729ba363bdce8c02126ef808c7f27d96
Anastasia Baturina <89099934219@mail.ru>
2020-11-29 21:11:47 +0300

    initial commit

---
+++ src/Bonus.java
@@ -0,0 +1,13 @@
+public class Bonus {
+    public static void main(String[] args) {
+        int balance = 8900;
+        int bonus;
+        if (balance > 1000) {
+            bonus = balance / 100;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println(bonus);
+    }
+}
